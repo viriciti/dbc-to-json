64265fab3db0a437db25145a7e11e1f0d70211fe
Floris van Lent <f.vanlent@viriciti.com>
2020-07-27 18:53:16 +0200

    Add functionality to get out the SG_ object

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 const splitCanId = (canId) => {
 
+	if(isNaN(canId)) throw new Error("CAN ID is not a number")
+
 	let isExtendedFrame = canId > 0xffff
 	let priority, pgn, source
 
@@ -11,7 +13,34 @@ const splitCanId = (canId) => {
 		pgn = canId
 	}
 
-	return {isExtendedFrame, priority, pgn, source}
+	return { isExtendedFrame, priority, pgn, source }
 }
 
-module.exports = { splitCanId }
+const extractSignalData = (line) => {
+	let multiplexer
+
+	if(line.length === 9 && line[3] === ":") {
+		[multiplexer] = line.splice(2, 1)
+	}
+
+	// TODO edge cases as warnings (return them as Array)
+	const [startBit, bitLength, littleEndian] = line[3].split(/[^\d]/)
+	const [factor, offset] = line[4].slice(1, -1).split(",")
+	const [min, max]       = line[5].slice(1, -1).split("|")
+
+	return {
+		name: line[1],
+		startBit: parseInt(startBit),
+		bitLength: parseInt(bitLength),
+		isLittleEndian: Boolean(littleEndian),
+		isSigned: line[3].endsWith("-"),
+		factor: parseFloat(factor),
+		offset: parseFloat(offset),
+		min: parseFloat(min),
+		max: parseFloat(max),
+		unit: line[6].slice(1, -1),
+		multiplexer
+	}
+}
+
+module.exports = { splitCanId, extractSignalData }
