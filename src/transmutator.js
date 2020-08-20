ae57c61d7bc7bbb0c1238221ee745fd0d80f3c0b
Sakif Surur <s.surur@viriciti.com>
2020-08-20 10:24:25 +0200

    fix: merge conflict issues

--- src/transmutator.js
+++ src/transmutator.js
@@ -2,7 +2,7 @@ const _              = require("underscore")
 const debug          = require("debug")("transmutator")
 const { snakeCase }  = require("snake-case")
 
-const { splitCanId, extractSignalData } = require("./utils")
+const { splitCanId, extractSignalData, extractValueData } = require("./utils")
 
 // TODO: remove empty lines without losing link to line number
 const parseDbc = (dbcString) => {
@@ -52,7 +52,7 @@ const parseDbc = (dbcString) => {
 
 				// Get data fields
 				let [, canId, name, dlc] = line
-				
+
 				if(isNaN(canId)) throw new Error(`CAN ID is not a number in the DBC file on line ${index + 1}`)
 
 				name  = name.slice(0, -1)
