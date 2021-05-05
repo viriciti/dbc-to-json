af3173a12ca49e3ec7785ff4cd6917bd11cc5430
Floris van Lent <f.vanlent@viriciti.com>
2021-05-05 12:06:20 +0200

    Also change min and max when switching unit from meters to kilometers

--- src/transmutator.js
+++ src/transmutator.js
@@ -144,6 +144,8 @@ const parseDbc = (dbcString, options = {}) => {
 									signalData.postfixImperial = "mi"
 									signalData.offset = signalData.offset / 1000 // TODO, log postprocessing events like these
 									signalData.factor = signalData.factor / 1000
+									signalData.factor = signalData.min    / 1000
+									signalData.factor = signalData.max    / 1000
 								} else {
 									signalData.postfixMetric = signalData.sourceUnit
 								}
